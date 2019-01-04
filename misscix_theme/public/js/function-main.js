0e53b1f6301553dde102e60dba019956550a9ea3
harpiya <yasir@harpiya.com>
2019-01-05 02:01:15 +0300

    up

--- misscix_theme/public/js/function-main.js
+++ misscix_theme/public/js/function-main.js
@@ -5,7 +5,7 @@
  * @Project: Harpiya Kurumsal Yönetim Sistemi
  * @Filename: function-main.js
  * @Last modified by:   developer
- * @Last modified time: 2019-01-04T23:46:05+03:00
+ * @Last modified time: 2019-01-05T02:00:58+03:00
  * @License: MIT License. See license.txt
  * @Copyright: Harpiya Yazılım Teknolojileri
  */
@@ -333,6 +333,14 @@ $(function(){
 	        menuHome6.toggleClass('pushmenu-open');
 	        $('.menu-mobile-left-content').hide();
 	        $('#slide-bar-category').hide();
+					frappe.call({
+									method: 'erpnext.shopping_cart.cart.get_shopping_cart_menu',
+									callback: function(r) {
+										if (r.message) {
+											$('.shopping-cart-nav').html(r.message);
+										}
+									}
+				});					
 	    });
 	    $(".wrappage").on("click", function() {
 	        $(this).removeClass('active');
