027262211be9cf3ae8b4518b3af16c4e87613c64
harpiya <yasir@harpiya.com>
2019-01-09 00:20:20 +0300

    ups

--- misscix_theme/hooks.py
+++ misscix_theme/hooks.py
@@ -4,7 +4,7 @@
 # @Project: Harpiya Kurumsal Yönetim Sistemi
 # @Filename: hooks.py
 # @Last modified by:   developer
-# @Last modified time: 2019-01-07T12:37:10+03:00
+# @Last modified time: 2019-01-09T00:19:49+03:00
 # @License: MIT License. See license.txt
 # @Copyright: Harpiya Yazılım Teknolojileri
 
@@ -35,8 +35,8 @@ website_context = {
 # app_include_js = "/assets/misscix_theme/js/misscix_theme.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/misscix_theme/css/misscix_theme.css"
-# web_include_js = "/assets/misscix_theme/js/misscix_theme.js"
+web_include_css = "/assets/misscix_theme/css/misscix-web.css"
+web_include_js = "/assets/misscix_theme/js/misscix.min.js"
 
 # Installation
 # ------------
